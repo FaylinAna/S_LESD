873008d49fb87384198ce24de40af356cf20511d
FaylinAna <ylexxna14@gmail.com>
2019-02-19 02:46:20 -0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Configuracion;
 
 class HomeController extends Controller
 {
-    //
+    public function allAlumno()
+    {
+        return Configuracion::all();
+    }
+
+    public function update_Configuracion(Request $request,Configuracion $Configuracion)
+    {
+        $Configuracion->update($request->all());
+        return response()->json($Configuracion);
+    }
+
+
 }
